7c086fd9297265529c306b3ad39cbf0a4a407c1f
pratik bapat <pb8424940758@gmail.com>
2020-11-15 20:33:04 +0530

    fresh start from scratch

--- app.js
+++ app.js
@@ -1,28 +1,9 @@
-// import Typed from "typed.js";
-const menu = document.querySelector("#mobile-menu");
-const menuLinks = document.querySelector(".navbar__menu");
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
 
 //Display Mobile Menu
 const mobileMenu = () => {
-  menu.classList.toggle("is-active");
-  menuLinks.classList.toggle("active");
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
 };
-
-menu.addEventListener("click", mobileMenu);
-
-// Typing Effect
-var options = {
-  strings: [
-    "Hi, I am Pratik Bapat.",
-    "I am a Quick Learner.",
-    "I Love to Develope.",
-    "I Love myself",
-  ],
-  backSpeed: 50,
-  typeSpeed: 50,
-  loop: true,
-  showCursor: false,
-  smartBackspace: true,
-  // fadeOut: true,
-};
-var typed = new Typed(".elements", options);
+menu.addEventListener('click', mobileMenu);
